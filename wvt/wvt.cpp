95b09300adc02ad033ae5a01cb30d7eb179ae779
Max DeLiso <maxdeliso@gmail.com>
2013-04-01 10:43:54 -0400

    license and comments

--- wvt/wvt.cpp
+++ wvt/wvt.cpp
@@ -1,3 +1,24 @@
+/*
+ * wvt.cpp - windows terminal emulator
+ *
+ * Copyright (c) 2013 Joseph M DeLiso
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <Windows.h>
 
 #include <cassert>
@@ -40,8 +61,8 @@ const int WIN_Context::windowInitialHeight = 316;
 
 int CALLBACK WinMain(
   HINSTANCE hInstance,
-  HINSTANCE hPrevInstance, 
-  LPSTR lpCmdLine, 
+  HINSTANCE hPrevInstance,
+  LPSTR lpCmdLine,
   int nCmdShow) {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -57,7 +78,7 @@ int CALLBACK WinMain(
 
     WIN_Context* winContext = new WIN_Context(hInstance);
 
-    if( winContext->registerWindowClass() && 
+    if( winContext->registerWindowClass() &&
       winContext->createMainWindow(nCmdShow) ) {
         winContext->messageLoop();
     } else {
@@ -83,49 +104,50 @@ void WIN_Context::messageLoop() {
 }
 
 BOOL WIN_Context::registerWindowClass() {
-  WNDCLASSEX wcx; 
+  WNDCLASSEX wcx;
 
-  wcx.cbSize = sizeof(wcx);     
-  wcx.style = CS_HREDRAW | CS_VREDRAW; 
-  wcx.lpfnWndProc = windowProcedure;  
+  wcx.cbSize = sizeof(wcx);
+  wcx.style = CS_HREDRAW | CS_VREDRAW;
+  wcx.lpfnWndProc = windowProcedure;
   wcx.cbClsExtra = 0;
-  wcx.cbWndExtra = 0; 
+  wcx.cbWndExtra = 0;
   wcx.hInstance = hInstance;
   wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-  wcx.hCursor = LoadCursor(NULL, IDC_ARROW); 
-  wcx.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);  
-  wcx.lpszMenuName =  NULL;   
-  wcx.lpszClassName = windowClassName;  
-  wcx.hIconSm = NULL; 
+  wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wcx.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+  wcx.lpszMenuName =  NULL;
+  wcx.lpszClassName = windowClassName;
+  wcx.hIconSm = NULL;
 
-  return RegisterClassEx(&wcx); 
-} 
+  return RegisterClassEx(&wcx);
+}
 
 BOOL WIN_Context::createMainWindow(int nCmdShow)  {
-  HWND hwnd = CreateWindowEx( 
+  HWND hwnd = CreateWindowEx(
     0,
-    windowClassName,       
-    windowTitleName,          
-    WS_OVERLAPPEDWINDOW, 
+    windowClassName,
+    windowTitleName,
+    WS_OVERLAPPEDWINDOW,
     CW_USEDEFAULT,
     CW_USEDEFAULT,
     windowInitialWidth,
     windowInitialHeight,
     (HWND) NULL,
     (HMENU) NULL,
-    hInstance, 
+    hInstance,
     (LPVOID) NULL);
 
   if (!hwnd) {
-    return FALSE; 
+    return FALSE;
   }
 
-  ShowWindow(hwnd, nCmdShow); 
-  UpdateWindow(hwnd); 
-  return TRUE; 
+  ShowWindow(hwnd, nCmdShow);
+  UpdateWindow(hwnd);
+
+  return TRUE;
 }
 
-LRESULT CALLBACK WIN_Context::windowProcedure( 
+LRESULT CALLBACK WIN_Context::windowProcedure(
   HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
     if(debugging()) {
